7d781aa246ce47f9e61cfdbcd565ae8d4dd5b7af
TWetmore34 <tjwetmore34@gmail.com>
2022-06-30 17:55:25 -0400

    search displays article name, date, subjects, and info properly

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,27 @@ function search(q){
     }).then(function (data){
         for (i=0;i<data.results.length;i++){
             let titleEl = document.createElement('div');
-            titleEl.classList.add('card-title');
+            titleEl.classList = 'card-title col-12 bg-secondary text-light rounded'
             titleEl.textContent = data.results[i].title;
-        resultEl.appendChild(titleEl);
+            resultEl.appendChild(titleEl);
+
+            // Create info elements
+            let infoEl = document.createElement('div');
+            let dateEl = document.createElement('p');
+            let subjectEl = document.createElement('p');
+            let descEl = document.createElement('p');
+            infoEl.classList = 'card-text bg-info';
+
+            // text info
+            dateEl.innerHTML = 'date: ' + data.results[i].timestamp;
+            subjectEl.innerHTML = 'subjects: ' + data.results[i].subject;
+            descEl.innerHTML = 'description: ' + data.results[i].description
+
+            // append children
+            resultEl.appendChild(infoEl);
+            infoEl.appendChild(dateEl);
+            infoEl.appendChild(subjectEl);
+            infoEl.appendChild(descEl);
         }
         console.log(data)
         
@@ -26,6 +44,7 @@ function search(q){
 
 buttonEl.addEventListener('click', function(e){
     e.preventDefault();
+    resultEl.children.textContent = '';
     let question = searchEl.value.trim();
     if(question){
         console.log(question)
